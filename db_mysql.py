67de34bb01d0829a6017180a3a7cf6653fc60232
Quyen Phan <200496653+quyenphanlyons@user.noreply.github.com>
2026-05-02 20:15:01 +0000

    add op2

--- db_mysql.py
+++ db_mysql.py
@@ -13,7 +13,7 @@ def connect():
         ssl={'ssl':{}}
     )
 
-
+# Option 1: Get speakers, their sessions and rooms
 def get_speakers_sessions(name):
     global conn
 
@@ -31,3 +31,46 @@ def get_speakers_sessions(name):
     cursor.execute(query, ("%" + name + "%",))
     return cursor.fetchall()
 
+# Option 2: Get attendees by company
+
+# the companyId exists?
+def company_exists(companyId):
+    global conn
+
+    if not conn:
+        connect()
+
+    query = """
+    "SELECT companyId
+    "FROM company
+    "WHERE companyId = %s
+    """
+    cursor = conn.cursor()
+    cursor.execute(query, (companyId,))
+    return cursor.fetchone()
+
+# get the attendees of a company
+def get_attendees(companyId):
+    global conn
+
+    if not conn:
+        connect()
+
+    query = """
+    SELECT  c.companyName,
+            a.attendeeName, 
+            a.attendeeDOB,
+            a.attendeeTitle,
+            s.speakerName,
+            r.roomName
+    FROM attendee a
+    JOIN company c ON a.attendeeCompanyId = c.companyId
+    JOIN registration re ON a.attendeeID = re.attendeeID
+    JOIN session s ON re.sessionID = s.sessionID
+    JOIN room r ON s.roomID = r.roomID
+    WHERE a.attendeeCompanyId = %s
+    """
+
+    cursor = conn.cursor()
+    cursor.execute(query, (companyId,))
+    return cursor.fetchall()
--- main.py
+++ main.py
@@ -13,6 +13,20 @@ def menu():
     print("6 - View Rooms")
     print("x - Exit")
 
+
+def main():
+    while True:
+        menu()
+        choice = input("Choice: ")
+
+        if choice == "1":
+            op1()
+        elif choice =="2":
+            op2()
+        elif choice =="x":
+            break
+
+# Option 1: Get speakers, their sessions and rooms
 def op1():
     name = input("Enter speaker name: ")
     results = db_mysql.get_speakers_sessions(name)
@@ -28,16 +42,32 @@ def op1():
         print(f"{speaker} | {session} | {room}")
           
 
-def main():
+# Option 2: Get attendees by company
+def op2():
+    comId = int(input("Enter company ID: "))
 
-    while True:
-        menu()
-        choice = input("Choice: ")
+    # companyId must be valid
+    if comId <= 0:
+        raise ValueError("Company ID must be a positive integer")
+    
+    if not db_mysql.company_exists(comId):
+        print(f"Company with Id {comId} does not exist.")
+        return
 
-        if choice == "1":
-            op1()
-        elif choice =="x":
-            break
+    results = db_mysql.get_attendees(comId)
+    company_name = db_mysql.get_company_name(comId)
+
+    print(f"{company_name} Attendees")
+
+    if not results:
+        print(f"No attendees found for {company_name}")
+        return
+
+    for row in results:
+        name, dob, session, speaker, room = row
+        print(f"{name} | {dob} | {session} | {speaker} | {room}")
+
+
 
 if __name__ == "__main__":
     main()
