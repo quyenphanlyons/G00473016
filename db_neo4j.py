3b8b8d00395bce9db94c9de7d4301965bd6dc274
Quyen Phan <200496653+quyenphanlyons@user.noreply.github.com>
2026-05-05 20:34:10 +0000

    add message to neo4j

--- db_neo4j.py
+++ db_neo4j.py
@@ -4,11 +4,15 @@ driver = None
 
 def connect():
     global driver
-    uri = "bolt://localhost:7687"
+    uri = "neo4j://localhost:7687"
     username = "neo4j"      
     password = "neo4jneo4j"
 
     driver = GraphDatabase.driver(uri, auth=(username, password))
+
+if __name__ == "__main__":
+    connect()
+    print("Connected to Neo4j")
 
 # Option 4: Get connected attendees
 def get_connected_attendees(attendeeID):
